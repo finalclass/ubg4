8bda54ef27ce5c10c33acdebefc04631a79d4b31
Szymon Wygnanski <s@finalclass.net>
2019-03-12 19:49:10 +0100

    release configuration

--- src/ubg4_app.erl
+++ src/ubg4_app.erl
@@ -9,13 +9,13 @@
 -export([stop/1]).
 
 -define(L(Msg), io:format("~b ~p~n", [?LINE, Msg])).
+-define(PRIVDIR, code:priv_dir(ubg4)).
 
 %% API.
 
 start(_Type, _Args) ->
-    %% erltl:compile("./priv/index.html"),
-    TemplateCompilationResult = erlydtl:compile_file("./priv/index.dtl", ubg4_templates_index),
-    Ubg4Data = ubg4_data:read_bible("./priv/pubg-utf8.xml"),
+    TemplateCompilationResult = erlydtl:compile_file(?PRIVDIR ++ "/index.dtl", ubg4_templates_index),
+    Ubg4Data = ubg4_data:read_bible(?PRIVDIR ++ "/pubg-utf8.xml"),
     ?L(TemplateCompilationResult),
     Dispatch = cowboy_router:compile([
                                       {'_', [
@@ -23,6 +23,7 @@ start(_Type, _Args) ->
                                              {"/:book/:chapter", ubg4_handler, [{bible, Ubg4Data}]}
                                             ]}
                                      ]),
+
     {ok, _} = cowboy:start_clear(http, [{port, 8080}], #{
                                                          env => #{dispatch => Dispatch}
                                                         }),
