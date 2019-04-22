936f66ce4c227514a19d699e5203a767f7f6b0dd
Szymon Wygnanski <s@finalclass.net>
2019-04-22 12:19:54 +0200

    fix issue with exporting from case

--- src/ubg4_handler.erl
+++ src/ubg4_handler.erl
@@ -17,13 +17,13 @@ init(Req, State) ->
             NoLayout = proplists:get_value(<<"noLayout">>, cowboy_req:parse_qs(Req), false),
             ResponseBody = case NoLayout of 
                                <<"true">> ->
-                                   ResponseBody = ubg4_templates:chapter([{chapter, Chapter}]);
+                                   ubg4_templates:chapter([{chapter, Chapter}]);
                                _ -> 
-                                   ResponseBody = ubg4_templates:index(
-                                                    [
-                                                     {books, Books},
-                                                     {chapter, Chapter}
-                                                    ])
+                                   ubg4_templates:index(
+                                     [
+                                      {books, Books},
+                                      {chapter, Chapter}
+                                     ])
                    end,
 
             ReqWithReply = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>},
